8454a70e01328f62203966df8e8e2118bd941965
Diegogl91 <garciadiego03@gmail.com>
2021-08-25 23:37:45 +0000

    code done

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let cartas = document.getElementById('cartas');
 let contenedorLog = document.getElementById('contenedorLog')
 let grupodeCartas = [];
+let cartasGeneradas = [];
 
 
-// let carta = document.getElementById('carta');
-function CrearCarta() {
+function CrearCarta(elemPadre, cardData) {
     let numeroDeCartas = document.getElementsByClassName('carta').length;
     let carta = document.createElement('div');
     carta.id = `carta-${numeroDeCartas}`;
@@ -15,24 +15,22 @@ function CrearCarta() {
     carta.style.borderRadius = "5px";
     carta.style.margin = "20px";
     carta.style.display = "inline-block";
-    cartas.appendChild(carta);
+    elemPadre.appendChild(carta);
     let pintaup = document.createElement('div');
     pintaup.className = "pintasuperior"
     pintaup.style.position = "relative";
     pintaup.style.top = "0px";
     pintaup.style.left = "5px";
     pintaup.style.fontSize = "30px";
-    let pintaSeleccionada = obtenerPintayColor();
-    pintaup.innerHTML = pintaSeleccionada.forma;
-    pintaup.style.color = pintaSeleccionada.color;
+    pintaup.innerHTML = cardData.pinta.forma;
+    pintaup.style.color = cardData.pinta.color;
     carta.appendChild(pintaup)
     let valor = document.createElement('div')
     valor.className = "valorcarta";
     valor.style.textAlign = "center";
     valor.style.paddingTop = "5px";
     valor.style.fontSize = "30px";
-   let valorSeleccionado = obtenerNumero();
-   valor.innerHTML = valorSeleccionado.valor;
+    valor.innerHTML = cardData.valor.valor;
     carta.appendChild(valor);
     let pintadown = document.createElement('div')
     pintadown.className = "pintainferior";
@@ -41,11 +39,18 @@ function CrearCarta() {
     pintadown.style.left = "-5px";
     pintadown.style.fontSize = "30px";
     pintadown.style.transform = "rotate(180deg)";
-    pintadown.innerHTML = pintaSeleccionada.forma;
-    pintadown.style.color = pintaSeleccionada.color;
-    carta.appendChild(pintadown)
-    return {valor:valorSeleccionado, pinta:pintaSeleccionada};
+    pintadown.innerHTML = cardData.pinta.forma;
+    pintadown.style.color = cardData.pinta.color;
+    carta.appendChild(pintadown);
+}
 
+function generarValores(num) {
+    let array = [];
+    for (let i = 0; i < num; i++) {
+        let cartaGenerada = { valor: obtenerNumero(), pinta: obtenerPintayColor()}
+        array.push(cartaGenerada);
+    }
+    return array;
 }
 
 function obtenerPintayColor() {
@@ -68,8 +73,8 @@ function obtenerPintayColor() {
     return pinta;
 }
 
-function obtenerNumero(){
-    let num = Math.floor(Math.random() * 12)+1;
+function obtenerNumero() {
+    let num = Math.floor(Math.random() * 12) + 1;
     let numeros = num;
     switch (num) {
         case 1:
@@ -85,54 +90,55 @@ function obtenerNumero(){
             numeros = "K";
             break;
     }
-    return {numero: num, valor:numeros};
+    return { numero: num, valor: numeros };
 }
 
-function repartirCartas(){
+function repartirCartas() {
     let numDeCartas = document.getElementById('nCartas').value;
-    let cartas = document.getElementById(`cartas`)
+    cartasGeneradas = generarValores(numDeCartas);
+    contenedorLog.innerHTML="";
     cartas.innerHTML = "";
-    for (let i=0;i<numDeCartas;i++){
-        grupodeCartas.push(CrearCarta())
+    for (let i = 0; i < numDeCartas; i++) {
+        grupodeCartas.push(CrearCarta(cartas, cartasGeneradas[i]))
     }
-    // console.log(grupodeCartas)
-    // console.log(bubbleSort(grupodeCartas))
 }
 
-window.onload = function(){
+window.onload = function () {
     let draw = document.getElementById('btncartas');
-    draw.addEventListener('click', function(){
+    draw.addEventListener('click', function () {
         repartirCartas()
     })
 
     let sort = document.getElementById('btnOrdenar')
-    sort.addEventListener('click',function(){
-        bubbleSort(grupodeCartas)
-    } )
+    sort.addEventListener('click', function () {
+        bubbleSort(cartasGeneradas)
+    })
 }
 
 
 const bubbleSort = (arr = []) => {
     let wall = arr.length - 1;
-    while( wall >= 0){
+    let track = 0;
+    while (wall >= 0) {
         let index = 0;
-        while(index < wall){
-            if(arr[index].valor.numero > arr[index + 1].valor.numero){
+        while (index < wall) {
+            if (arr[index].valor.numero > arr[index + 1].valor.numero) {
+                let iteracion = document.createElement('div');
+                let tracker = document.createElement('div');
+                tracker.innerHTML = track;
+                tracker.className = `iteracion`;
+                iteracion.appendChild(tracker);
                 let aux = arr[index + 1];
                 arr[index + 1] = arr[index];
                 arr[index] = aux;
                 let nuevoContenedor = document.createElement("div");
-                contenedorLog.appendChild(nuevoContenedor);
-                nuevoContenedor.style.height = "80px";
-                nuevoContenedor.style.marginBottom = "10px";
-                let contenedorNumero = document.createElement("div");
-                contenedorNumero.style.float = "left";
-                contenedorNumero.style.width = "20px";
-                nuevoContenedor.appendChild(contenedorNumero);
-                let numeroIteracion = document.createElement("span");
-                numeroIteracion.innerHTML = index;
-                contenedorNumero.appendChild(numeroIteracion);
-                
+                nuevoContenedor.className = `arregloDeCartas`
+                for (let i = 0; i < arr.length; i++) {
+                    CrearCarta(nuevoContenedor, cartasGeneradas[i]);
+                }
+                iteracion.appendChild(nuevoContenedor);
+                contenedorLog.appendChild(iteracion); 
+                track++;
             }
             index++;
         }
@@ -140,7 +146,6 @@ const bubbleSort = (arr = []) => {
     }
     return arr;
 }
-
 
 
 
