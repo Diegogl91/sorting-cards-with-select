3ca7098768c14900986299666f733ffa7174895a
Diegogl91 <garciadiego03@gmail.com>
2021-08-25 14:33:47 +0000

    estilo de carta

--- main.js
+++ main.js
@@ -3,3 +3,24 @@ let pintas = ['corazon', 'diamante', 'picas', 'trebol'];
 
 let carta = document.getElementById('carta');
 carta.style.background = "#FFFFFF";
+carta.style.width = "80px";
+carta.style.height = "120px";
+carta.style.borderRadius = "5px";
+carta.style.margin = "20px"
+let valor = document.getElementById('valorcarta');
+valor.style.textAlign = "center";
+valor.style.paddingTop = "5px";
+valor.style.fontSize = "30px";
+let pintaup = document.getElementById('pintasuperior')
+pintaup.style.position = "relative";
+pintaup.style.top = "0px"
+pintaup.style.left = "5px"
+pintaup.style.fontSize = "30px"
+let pintadown = document.getElementById('pintainferior');
+pintadown.style.position = "relative"
+pintadown.style.top = "15px";
+pintadown.style.left = "-5px";
+pintadown.style.fontSize = "30px"
+pintadown.style.transform = "rotate(180deg)"
+
+
